477b07729f8f54f4b15db7c0847a03bc4ce643de
cgmossa <cgmossa@gmail.com>
2023-04-17 20:26:00 +0200

    Started on xtasks for creating the mock libgcc and for moving r-headers to repository

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,64 @@
-use std::path::PathBuf;
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
+use std::path::{Path, PathBuf};
 
-fn main() {
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Command {
+    LibraryGccMock,
+    CopyRHeaders,
+}
+
+fn main() -> Result<()> {
     // println!("Hello, world!");
     // println!("CARGO_MANIFEST_DIR: {}", std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let xtask_directory = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let xtask_directory: PathBuf = xtask_directory.into();
     let workspace_root = xtask_directory.parent().unwrap();
-    todo!();
-    //TODO:
+
+    let args = Args::parse();
+    match args.command {
+        Command::LibraryGccMock => libgcc_mock()?,
+        Command::CopyRHeaders => copy_r_headers(workspace_root),
+        _ => {
+            todo!()
+        }
+    };
+    Ok(())
+}
+
+fn libgcc_mock() -> Result<()> {
+    use std::env;
+    use std::fs;
+
+    // let out_dir = env::var("OUT_DIR")?;
+    let libgcc_var =
+        env::var("LIBRARY_PATH").context("Set `LIBRARY_PATH` on your System / User")?;
+    if libgcc_var.is_empty() {
+        anyhow::bail!("Environment variable `LIBRARY_PATH` cannot be empty.")
+    }
+    // create a directory in an arbitrary location (e.g. libgcc_mock)
+    let libgcc_mock_path = Path::new(&libgcc_var);
+    if !libgcc_mock_path.exists() {
+        fs::create_dir(libgcc_mock_path)?
+    }
+    if !libgcc_mock_path.join("libgcc_eh.a").exists() {
+        fs::File::create(libgcc_mock_path.join("libgcc_eh.a"))?;
+        fs::File::create(libgcc_mock_path.join("libgcc_s.a"))?;
+    }
+    Ok(())
+}
+
+fn copy_r_headers(workspace_root: &Path) {
     /*
+
     // copy R headers and binaries over to the repository
     fs_extra::dir::remove("r")?;
     std::fs::create_dir_all("r")?;
