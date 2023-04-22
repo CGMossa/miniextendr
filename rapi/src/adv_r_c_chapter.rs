a36864d0bdd0f9ce82ebc7c98dfa2c405a482aa3
cgmossa <cgmossa@gmail.com>
2023-04-22 08:54:20 +0200

    get started on the `rapi`

---
+++ rapi/src/adv_r_c_chapter.rs
@@ -0,0 +1,32 @@
+use rsys::{protect_macros::*, *};
+
+/*
+
+SEXP add(SEXP a, SEXP b) {
+  SEXP result = PROTECT(allocVector(REALSXP, 1));
+  REAL(result)[0] = asReal(a) + asReal(b);
+  UNPROTECT(1);
+
+  return result;
+}
+*/
+
+unsafe fn add(a: SEXP, b: SEXP) -> SEXP {
+  let mut result: SEXP = PROTECT(Rf_allocVector(REALSXP, 1));
+  *REAL(result) = Rf_asReal(a) + Rf_asReal(b);
+  UNPROTECT(1);
+  result
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_add() {
+    let a = 46;
+    let b = -4;
+
+    // println!("{}", *add(a, b));
+  }
+}
